0fd5a2b2b502323413678a210489287b571be0a9
César Castañón <cesar.castanon@piperlab.es>
2022-11-02 16:53:32 +0100

    [ADD] app base structure implemented

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+from app.celery import create_celery
+from app.users.routes import users_router
+
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+
+    app.include_router(users_router)
+
+    return app
+
+
+app = create_app()
+
+celery = create_celery()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
